0fb70b050b8ac476cde8ea5fb0d2a1854892e6bc
tommy771004 <tommy771004@gmail.com>
2026-05-19 11:57:23 +0800

    [Batch] 批次上傳: 更新了 10 個檔案 - migrations/0004_tranquil_prism.sql

--- migrations/0004_tranquil_prism.sql
+++ migrations/0004_tranquil_prism.sql
@@ -2,5 +2,4 @@ ALTER TABLE "itinerary_nodes" ADD COLUMN "timestamp" timestamp;--> statement-bre
 ALTER TABLE "itinerary_nodes" ADD COLUMN "description" text;--> statement-breakpoint
 ALTER TABLE "itinerary_nodes" ADD COLUMN "is_visited" boolean DEFAULT false NOT NULL;--> statement-breakpoint
 ALTER TABLE "itinerary_nodes" ADD COLUMN "transport_to_next" text;--> statement-breakpoint
-ALTER TABLE "itinerary_nodes" ADD COLUMN "image_url" text;--> statement-breakpoint
-ALTER TABLE "trip_travel_facts" ADD CONSTRAINT "trip_travel_facts_trip_id_trips_id_fk" FOREIGN KEY ("trip_id") REFERENCES "public"."trips"("id") ON DELETE no action ON UPDATE no action;
+ALTER TABLE "itinerary_nodes" ADD COLUMN "image_url" text;
